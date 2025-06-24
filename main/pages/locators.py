0317679fdd2ac0062c2d5340e921760941547369
Artemy <aconfear@gmail.com>
2025-06-24 14:06:16 +0300

    Move to page

--- main/pages/locators.py
+++ main/pages/locators.py
@@ -1,5 +1,11 @@
 from selenium.webdriver.common.by import By
 
+from main.pages.base_page import BasePage
+
 
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators(BasePage):
+    FORM_LOG_ON = (By.ID, "login_form")
+    FORM_SIGN_IN = (By.ID, "register_form")
